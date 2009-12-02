651c00dafed411201f4d0bfb5ddb7e165000b2af
Mark Meves <mark.meves@gmail.com>
2009-12-02 18:04:51 -0500

    first ramaze controller made! yay

--- first.rb
+++ first.rb
@@ -3,9 +3,19 @@ require 'rubygems'
 require 'ramaze'
 
 class MainController < Ramaze::Controller
+  
   def index 
     'yo'
   end
 end
+
+class BlogController < Ramaze::Controller
+  map '/blogs'
+  
+  def index
+    'ramaze still rocks'
+  end
+end
+
 
 Ramaze.start
